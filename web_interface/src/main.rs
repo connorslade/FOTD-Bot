799f6f43e0e6c1df710ab8fae695ac13444e40a7
Connor Slade <connor@connorcode.com>
2021-08-31 23:19:04 -0400

    ✉️ Get email and why

--- web_interface/src/main.rs
+++ web_interface/src/main.rs
@@ -1,6 +1,7 @@
 use afire::*;
 use std::fs;
 
+/// Dir to find files to serve
 const DATA_DIR: &str = "static";
 
 fn main() {
@@ -8,6 +9,7 @@ fn main() {
 
     Logger::attach(&mut server, Logger::new(Level::Info, None, true));
 
+    // Serve Static files from DATA_DIR
     server.all(|req| {
         let mut path = format!("{}/{}", DATA_DIR, req.path.replace("/..", ""));
         // Add Index.html if path ends with /
@@ -43,16 +45,23 @@ fn main() {
         };
     });
 
+    // Process Unsub Requests
     server.route(Method::POST, "/unsubscribe/real", |req| {
         let query = Query::from_body(req.body);
-        println!(
-            "email: {} - why: {}",
-            decode_url_chars(&query.get("email").unwrap_or_default()),
-            decode_url_chars(&query.get("why").unwrap_or_default())
-        );
+
+        let email = match &query.get("email") {
+            Some(email) => decode_url_chars(email),
+            None => return Response::new(400, "Invalid Email", vec![]),
+        };
+
+        let why = match &query.get("why") {
+            Some(why) => decode_url_chars(why),
+            None => return Response::new(400, "Invalid Reason", vec![]),
+        };
+
         Response::new(
             200,
-            "N O S E",
+            "i hate you.\n - FOTD BOT",
             vec![Header::new("Content-Type", "text/plain")],
         )
     });
@@ -61,6 +70,7 @@ fn main() {
     server.start();
 }
 
+/// Get the type MMIE content type of a file from its extension
 fn get_type(path: &str) -> &str {
     match path.split(".").last() {
         Some(ext) => match ext {
@@ -80,6 +90,7 @@ fn get_type(path: &str) -> &str {
     }
 }
 
+/// i dont even know...
 fn append_frag(text: &mut String, frag: &mut String) {
     if !frag.is_empty() {
         let encoded = frag
@@ -93,6 +104,7 @@ fn append_frag(text: &mut String, frag: &mut String) {
     }
 }
 
+/// Decode URL encoded strings
 fn decode_url_chars(text: &str) -> String {
     let mut output = String::new();
     let mut encoded_ch = String::new();
@@ -109,11 +121,13 @@ fn decode_url_chars(text: &str) -> String {
     output
 }
 
+/// Holds key value pairs from Query Strings
 struct Query {
     data: Vec<[String; 2]>,
 }
 
 impl Query {
+    /// Create a new Query from a Form POST body
     fn from_body(body: String) -> Query {
         let mut data = Vec::new();
 
@@ -133,6 +147,7 @@ impl Query {
         Query { data }
     }
 
+    /// Get a value from a key
     fn get(&self, key: &str) -> Option<String> {
         for i in self.data.clone() {
             if &i[0] == key {
