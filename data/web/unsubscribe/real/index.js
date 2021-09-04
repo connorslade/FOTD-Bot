ecf8e5f6eeb5a89f799fc98b4cb5404e8747cc09
Connor Slade <connor@connorcode.com>
2021-09-03 22:23:13 -0400

    📧 Unsub Email Loading Animation

---
+++ data/web/unsubscribe/real/index.js
@@ -0,0 +1,11 @@
+// Add another Event Listener
+document.getElementById("submit").onclick = () => {
+  document.getElementById("send").hidden = false;
+
+  let rep = 0;
+  setInterval(() => {
+    if (rep <= 16) document.getElementById("send").innerText = `Sending Email [=${'='.repeat(rep)}>]`;
+    else document.getElementById("send").innerText = `This is takeing a while...`;
+    rep++;
+  }, 250);
+};
