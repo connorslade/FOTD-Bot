56f8b0c0e274edf9a063965a0e71aa73b683eae8
Connor Slade <connor@connorcode.com>
2021-09-05 12:21:36 -0400

    🍦 Add favicon

--- src/web/routes/serve_static.rs
+++ src/web/routes/serve_static.rs
@@ -8,7 +8,8 @@ const DATA_DIR: &str = "data/web";
 
 pub fn add_route(server: &mut afire::Server) {
     server.all(|req| {
-        let mut path = format!("{}/{}", DATA_DIR, req.path.replace("/..", ""));
+        let mut path = format!("{}{}", DATA_DIR, req.path.replace("/..", ""));
+
         // Add Index.html if path ends with /
         if path.ends_with('/') {
             path.push_str("index.html");
@@ -20,19 +21,19 @@ pub fn add_route(server: &mut afire::Server) {
         }
 
         // Try to read File
-        match fs::read_to_string(&path) {
+        match fs::read(&path) {
             // If its found send it as response
-            Ok(content) => Response::new(
+            Ok(content) => Response::new_raw(
                 200,
-                &content,
+                content,
                 vec![Header::new("Content-Type", common::get_type(&path))],
             ),
 
             // If not send 404.html
-            Err(_) => Response::new(
+            Err(_) => Response::new_raw(
                 404,
-                &fs::read_to_string(format!("{}/404.html", DATA_DIR))
-                    .unwrap_or_else(|_| "Not Found :/".to_string()),
+                fs::read(format!("{}/404.html", DATA_DIR))
+                    .unwrap_or_else(|_| "Not Found :/".as_bytes().to_owned()),
                 vec![Header::new("Content-Type", "text/html")],
             ),
         }
