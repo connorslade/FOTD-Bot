06be286cb153bc61112fe9d0428193ae08c366db
Connor Slade <connor@connorcode.com>
2021-11-28 17:32:07 -0500

    🏀 Multi Webhook Support

--- src/webhook.rs
+++ src/webhook.rs
@@ -1,9 +1,12 @@
+use simple_config_parser::Config;
+
 pub enum Service {
     Discord,
     Slack,
 }
 
 pub struct Webhook {
+    pub id: String,
     pub service: Service,
     pub token: String,
     pub channel: String,
@@ -20,8 +23,9 @@ impl Service {
 }
 
 impl Webhook {
-    pub fn new(service: Service, token: String, channel: String) -> Webhook {
+    pub fn new(id: String, service: Service, token: String, channel: String) -> Webhook {
         Webhook {
+            id,
             service,
             token,
             channel,
@@ -102,6 +106,36 @@ impl Webhook {
             }
         }
     }
+}
+
+pub fn parse_config(config: &Config) -> Vec<Webhook> {
+    let mut out = Vec::new();
+    for i in &config.data {
+        if !i[0].starts_with("webhook_") {
+            continue;
+        }
+
+        let id = i[0].split("webhook_").nth(1).unwrap_or("None").to_owned();
+        if let Some(j) = parse_line(i[1].clone(), id) {
+            out.push(j);
+        }
+    }
+    out
+}
+
+fn parse_line(line: String, id: String) -> Option<Webhook> {
+    let mut parts = line.split(',');
+
+    let service = parts.next()?.trim();
+    let channel = parts.next()?.trim().to_owned();
+    let token = parts.next()?.trim().to_owned();
+
+    Some(Webhook::new(
+        id,
+        Service::from_string(service)?,
+        token,
+        channel,
+    ))
 }
 
 // TODO: Use a macro for the send function service options
