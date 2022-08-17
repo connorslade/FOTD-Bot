3d30a3483d0a67d9b4a45e20e0e0aad48ea08356
Connor Slade <connor@connorcode.com>
2022-08-17 10:57:09 -0700

    Remove unsafe blocks for avoding the borrow checker

--- src/web/routes/sub.rs
+++ src/web/routes/sub.rs
@@ -1,30 +1,23 @@
 use afire::*;
 use rand::Rng;
 use std::collections::HashMap;
+use std::fmt::Write;
 use std::fs;
 use std::sync::Arc;
-use std::fmt::Write;
 
-use crate::{common::common, email::Auth, web::quick_email, App};
+use crate::{common::common, web::quick_email, App};
 
 /// Dir to find files to serve
 const DATA_DIR: &str = "data/web";
 
-static mut AUTH: Option<Auth> = None;
-static mut BASE_URL: Option<String> = None;
-static mut TEMPLATE_PATH: Option<String> = None;
-static mut USER_PATH: Option<String> = None;
 static mut SUB_CODES: Option<HashMap<String, String>> = None;
 
 pub fn attach(server: &mut afire::Server, app: Arc<App>) {
     unsafe {
-        AUTH = Some(app.config.web_auth.clone());
-        BASE_URL = Some(app.config.web_url.clone());
-        TEMPLATE_PATH = Some(app.config.template_path.clone());
-        USER_PATH = Some(app.config.user_path.clone());
         SUB_CODES = Some(HashMap::new());
     }
 
+    let aapp = app.clone();
     server.route(Method::POST, "/subscribe", move |req| {
         let query = Query::from_body(req.body_string().unwrap()).unwrap();
 
@@ -40,8 +33,7 @@ pub fn attach(server: &mut afire::Server, app: Arc<App>) {
         };
 
         // If email is already subscribed dont send email etc.
-        let content = fs::read_to_string(unsafe { USER_PATH.clone() }.unwrap_or_default())
-            .unwrap_or_default();
+        let content = fs::read_to_string(&aapp.config.user_path).unwrap_or_default();
         if content.contains(&email) {
             return Response::new().text("Your Already Subscribed!\nNo need to subscribe again!");
         }
@@ -62,24 +54,23 @@ pub fn attach(server: &mut afire::Server, app: Arc<App>) {
                 .unwrap()
                 .insert(random_chars.clone(), email.clone());
         }
-        let mut confirm_url = unsafe { BASE_URL.clone() }
-            .unwrap_or_else(|| "https://www.youtube.com/watch?v=mKwj3efLxbc".to_string());
-        confirm_url.push_str(&format!("/subscribe/confirm?code={}", random_chars));
+        let confirm_url = &format!(
+            "{}/subscribe/confirm?code={}",
+            aapp.config.web_url, random_chars
+        );
 
         // Try to read File
-        let to_send = match fs::read_to_string(format!(
-            "{}/subscribe.html",
-            unsafe { TEMPLATE_PATH.clone() }.unwrap_or_default()
-        )) {
-            Ok(content) => content,
-            Err(_) => "Subscribe: {{URL}}".to_string(),
-        };
+        let to_send =
+            match fs::read_to_string(format!("{}/subscribe.html", aapp.config.template_path)) {
+                Ok(content) => content,
+                Err(_) => "Subscribe: {{URL}}".to_string(),
+            };
 
         quick_email(
-            unsafe { AUTH.as_ref().unwrap() },
+            &aapp.config.web_auth,
             email.clone(),
             "FOTD BOT Subscription".to_string(),
-            to_send.replace("{{URL}}", &confirm_url),
+            to_send.replace("{{URL}}", confirm_url),
         );
 
         Response::new()
@@ -91,7 +82,8 @@ pub fn attach(server: &mut afire::Server, app: Arc<App>) {
             .header("Content-Type", "text/html")
     });
 
-    server.route(Method::GET, "/subscribe/confirm/real", |req| {
+    let aapp = app;
+    server.route(Method::GET, "/subscribe/confirm/real", move |req| {
         let code = match req.query.get("code") {
             Some(code) => {
                 if code.is_empty() {
@@ -117,20 +109,19 @@ pub fn attach(server: &mut afire::Server, app: Arc<App>) {
         unsafe { SUB_CODES.as_mut() }.unwrap().remove(&code);
 
         // Add User to 'database'
-        let mut user_file =
-            match fs::read_to_string(unsafe { USER_PATH.clone() }.unwrap_or_default()) {
-                Ok(content) => content.replace('\r', ""),
-                Err(_) => return Response::new().status(500).text("Internal Error..."),
-            };
+        let mut user_file = match fs::read_to_string(&aapp.config.user_path) {
+            Ok(content) => content.replace('\r', ""),
+            Err(_) => return Response::new().status(500).text("Internal Error..."),
+        };
 
         // Add user to file only if not already in file
         if !user_file.contains(&email) {
-            write!(user_file, "\n{}", email);
+            write!(user_file, "\n{}", email).unwrap();
             user_file = user_file.replace("\n\n", "\n");
             if user_file.starts_with('\n') {
                 user_file.remove(0);
             }
-            fs::write(unsafe { USER_PATH.clone() }.unwrap_or_default(), user_file).unwrap();
+            fs::write(&aapp.config.user_path, user_file).unwrap();
         }
 
         Response::new()
